61e6920c6766be17e0a7bdef6d9a7cedccae6b10
Bronson Spies <spiesbronson@gmail.com>
2020-10-29 15:33:49 -0600

    spring setup

---
+++ GameBored/src/main/java/com/skilldistillery/gamebored/GameBoredApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.gamebored;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBoredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameBoredApplication.class, args);
+	}
+
+}
---
+++ GameBored/src/test/java/com/skilldistillery/gamebored/GameBoredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.gamebored;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameBoredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
