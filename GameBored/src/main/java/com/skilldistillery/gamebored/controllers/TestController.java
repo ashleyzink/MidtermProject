cc65a873e5c9e8bd2ccd693d36ae3dc40bf1f30a
Kayalyn Edmeade <kjedmeade@gmail.com>
2020-10-29 16:30:44 -0700

    Test Boot Spring MVC

---
+++ GameBored/src/main/java/com/skilldistillery/gamebored/controllers/TestController.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.gamebored.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	
+	@RequestMapping(path ="test.do")
+	public String home() {
+		return "test";
+	}
+}
