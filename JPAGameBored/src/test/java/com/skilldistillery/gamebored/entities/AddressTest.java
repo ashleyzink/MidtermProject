a40b2cf26101b520d19779b6e23fbdf989e2767b
Pierce <pierce.steward@gmail.com>
2020-10-30 15:01:56 -0600

    address_genre_publisher_added

---
+++ JPAGameBored/src/test/java/com/skilldistillery/gamebored/entities/AddressTest.java
@@ -0,0 +1,52 @@
+package com.skilldistillery.gamebored.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AddressTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Address address;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("GameBoredPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		address = em.find(Address.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		address = null;
+	}
+
+	@Test
+	void test_Address_entity_mapping() {
+		assertNotNull(address);
+		assertEquals("Aurora", address.getCity());
+		assertEquals("CO", address.getStateAbbreviation());
+		assertEquals("123 Sesame St", address.getStreet());
+		assertEquals("80013", address.getZipCode());
+	}
+
+}
