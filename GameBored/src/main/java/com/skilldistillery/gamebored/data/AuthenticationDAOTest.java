0fb4b8c2c447b08cfa25aa6b588780a2c1163628
Kayalyn Edmeade <kjedmeade@gmail.com>
2020-10-31 12:20:31 -0700

    changes

--- GameBored/src/main/java/com/skilldistillery/gamebored/data/AuthenticationDAOTest.java
+++ GameBored/src/main/java/com/skilldistillery/gamebored/data/AuthenticationDAOTest.java
@@ -1,17 +1,33 @@
 package com.skilldistillery.gamebored.data;
 
+import java.util.List;
+
+import com.skilldistillery.gamebored.entities.User;
+
 public class AuthenticationDAOTest {
-//	public static void main(String[] args) {
+		
+	public static void main(String[] args) {
+		AuthenticationDAOTest aTest = new AuthenticationDAOTest();
+		aTest.run();
+	}
+		
+	public void run() {
+//	AuthenticationDAO adao = new AuthenticationDAOImpl() ;
 //		
+//	User user = new User(1, "admin", "password", 1, "Game", "Bored", "adminuser@gamebored.com", Address());
 //	
-//	User user
-//	film.setTitle("hello");
-//	film.setRentalRate(2);
-//	film.setReplacementCost(12.95);
-//	Actor actor = new Actor(0, "Troy", "McClure");
-//	actor.addFilm(film);
-//	dao.createActorAndFilm(actor, film);
+//	
+//	//Create Tests for AuthenticationDAOImpl Here
+//	User userToDatabase = adao.create(user);
+//	System.out.println(userToDatabase);
+//	System.out.println(adao.listAll());
+//	System.out.println(adao.isEmailUnique(email));
+//	System.out.println(adao.isValidUser(user));
+//	System.out.println(adao.findByEmail(email));
+//	System.out.println(adao.isEmailUnique(email));
 //}
-//
+
 
 }
+}
+
---
+++ GameBored/src/main/webapp/WEB-INF/registrationpage.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title> Registration Page </title>
+</head>
+<body>
+<div>
+
+
+<h1>REGISTER</h1>
+</form> 
+<form action="registration.do" method="POST">
+ 
+<table>
+<tr><td> Username: <input type="text" name="username" required="required" /></td></tr> 
+<tr><td> Password <input type="text" name="password" required="required"/></td></tr> 
+<tr><td> First Name <input type="text" name="firstName" required="required"/></td></tr> 
+<tr><td> Last Name <input type="text" name="lastName" required="required"/></td></tr> 
+<tr><td> Email <input type="text" name="email" required="required"/></td></tr> 
+<tr><td> Address ? <input type="text" name="address" required="required"/></td></tr> 
+<tr><td> <input type="submit" value="Register" /></td></tr> 
+ </table>
+ </form>
+
+
+
+</body>
+</html>
