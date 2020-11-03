ebab5291c6b2b1b8b0a81fb711c3bd22375a3ada
Bronson Spies <spiesbronson@gmail.com>
2020-11-03 11:27:58 -0700

    change to user controller

--- GameBored/src/main/java/com/skilldistillery/gamebored/controllers/UserController.java
+++ GameBored/src/main/java/com/skilldistillery/gamebored/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.skilldistillery.gamebored.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -57,7 +59,8 @@ public class UserController {
 	}
 	
 	@RequestMapping("addBoardGameComment.do")
-	public String addBoardgameComment(BoardGameComment comment, Model model) {
+	public String addBoardgameComment(HttpSession session, BoardGameComment comment, Model model) {
+		comment.setUser((User)session.getAttribute("loggedInUser"));
 		BoardGameComment cc = bgDao.create(comment);
 		model.addAttribute("comment", cc);
 		return "homepage";
